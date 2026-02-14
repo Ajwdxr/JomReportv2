a10f3752be6949dcb08d7874bfd74806bbbaf7b8
unknown <ajwadxara99@gmail.com>
2026-02-14 11:57:08 +0800

    carian fix v5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home({
   }
 
   if (searchQuery) {
-    query = query.or(`title.ilike.%${searchQuery}%,description.ilike.%${searchQuery}%`)
+    query = query.or(`title.ilike.%${searchQuery}%,description.ilike.%${searchQuery}%,category.ilike.%${searchQuery}%`)
   }
 
   const { data: reports, error } = await query
